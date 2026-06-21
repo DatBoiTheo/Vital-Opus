af813d205a793f4be6e2523278de14a75aaf8568
theod_nerrud8 <theodorhedhman@gmail.com>
2026-06-22 00:07:12 +0200

    Cleaned up code. This is the "1st version", the template that will be expanded upon to create the mod.

---
+++ src/main/java/net/dbt/vitalopus/VitalOpus.java
@@ -0,0 +1,32 @@
+package net.dbt.vitalopus;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import org.slf4j.Logger;
+
+@Mod(VitalOpus.MOD_ID)
+public class VitalOpus {
+    public static final String MOD_ID = "vitalopus";
+    public static final Logger LOGGER = LogUtils.getLogger();
+    public VitalOpus(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+        NeoForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+
+    }
+
+    // Add the example block item to the building blocks tab
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+
+    }
+}
